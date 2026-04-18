fcf169186fed8749d75d6d2d28fca20ca38fd3c9
quocanh11103 <quocanh11103@gmail.com>
2026-04-18 10:45:44 +0700

    upload source lam quen Java

---
+++ HelloWorld/src/communityuni/com/FirstProgram.java
@@ -0,0 +1,11 @@
+package communityuni.com;
+
+public class FirstProgram {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Xin chào các Thím");
+
+	}
+
+}
