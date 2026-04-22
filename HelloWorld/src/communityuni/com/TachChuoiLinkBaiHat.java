86d18f632d89aad16614766a657a9ed2f42758f3
quocanh11103 <quocanh11103@gmail.com>
2026-04-22 10:18:40 +0700

    code tach ten file bai hat

--- HelloWorld/src/communityuni/com/TachChuoiLinkBaiHat.java
+++ HelloWorld/src/communityuni/com/TachChuoiLinkBaiHat.java
@@ -4,6 +4,7 @@ public class TachChuoiLinkBaiHat {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+	try {
 		String nguon = "D:/music/bolero/longme.mp3";
         StringBuilder sb = new StringBuilder(nguon);
 
@@ -19,7 +20,18 @@ public class TachChuoiLinkBaiHat {
         int dotIndex = sbFile.lastIndexOf(".");
         String TenOnly = sbFile.substring(0, dotIndex);
         System.out.println("Tên không đuôi: " + TenOnly);
+	}
+	catch(Exception ex)
+	{
+		ex.printStackTrace();
+	}
+	
+	finally
+	{
+		System.out.println("finally");
 
+	}
+	
 	}
 
 }
