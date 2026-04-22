fbc9f751972559b7d9c060e64514e74c7eec2da0
quocanh11103 <quocanh11103@gmail.com>
2026-04-22 10:13:33 +0700

    source tach file bai hat

---
+++ HelloWorld/src/communityuni/com/TachChuoiLinkBaiHat.java
@@ -0,0 +1,25 @@
+package communityuni.com;
+
+public class TachChuoiLinkBaiHat {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String nguon = "D:/music/bolero/longme.mp3";
+        StringBuilder sb = new StringBuilder(nguon);
+
+        
+        int GachCuoi = sb.lastIndexOf("/");
+        String TenFile = sb.substring(GachCuoi + 1);
+        System.out.println("Tên file: " + TenFile);
+
+       
+        StringBuilder sbFile = new StringBuilder(TenFile);
+
+       
+        int dotIndex = sbFile.lastIndexOf(".");
+        String TenOnly = sbFile.substring(0, dotIndex);
+        System.out.println("Tên không đuôi: " + TenOnly);
+
+	}
+
+}
