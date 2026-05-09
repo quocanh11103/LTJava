6a6317d82d89ebeb47de74afb858e5ff7f72308c
quocanh11103 <quocanh11103@gmail.com>
2026-05-09 09:23:43 +0700

    Xây dựng chương trình theo mô hình lớp được cho sẵn

---
+++ HelloWorld/src/communityuni/com/BaiTH_Xay_Dung_Chuong_Trinh_Theo_Mo_Hinh_Lop.java
@@ -0,0 +1,11 @@
+package communityuni.com;
+
+public class BaiTH_Xay_Dung_Chuong_Trinh_Theo_Mo_Hinh_Lop {
+
+	public static void main(String[] args) {
+	//Xây dựng chương trình theo mô hình lớp cơ bản
+		
+
+	}
+
+}
